f1379948d54b2a148c7d7f7537b1f1460c12858c
Kory Campbell <korycampbell@Dwights-MBP.home>
2015-11-20 02:30:21 -0500

    95% Done w/ First Iteration

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ User.destroy_all
 
 users = User.create([{name:'Mike', age:23}, {name:"John", age:25}])
 
-bookshelves = Bookshelf.create([{genre:'Sports', name:'Mikes Sports Bookshelf', user_id:1}, {genre:'Non-Fiction', name:'Johns Biography Bookshelf', user_id:2}])
+bookshelves = Bookshelf.create([{genre:'Books on Sports', name:'Mikes Sports Bookshelf', user_id:1}, {genre:'Autobiographes', name:'Johns Biography Bookshelf', user_id:2}])
 
-books = Book.create([{title:'Sports Book', publication_date:1996, bookshelf_id:1}, {title:'Malcolm X Bio', publication_date:1965, bookshelf_id:2}])
+books = Book.create([{title:'The Complete Book on Sports Betting', publication_date:1996, bookshelf_id:1}, {title:'The Autobiography of Malcolm X', publication_date:1965, bookshelf_id:2}])
