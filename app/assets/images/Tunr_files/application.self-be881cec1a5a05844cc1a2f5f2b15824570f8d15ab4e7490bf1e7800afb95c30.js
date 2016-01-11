e0abe7df56128da50e8dd81d33a2fa8e00bb0659
Kory Campbell <korycampbell@Dwights-MBP.home>
2016-01-11 18:00:14 -0500

    updated app w/ books

---
+++ app/assets/images/Tunr_files/application.self-be881cec1a5a05844cc1a2f5f2b15824570f8d15ab4e7490bf1e7800afb95c30.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+
+$(document).ready(function(){
+  $(".test_ajax_get").on("click", function(){
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: "http://localhost:3000/artists"
+    }).done(function(response) {
+      console.log(response);
+    }).fail(function(response){
+      console.log("Ajax get request failed.");
+    })
+  })
+})
